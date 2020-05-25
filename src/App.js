68750d33cdec63bd3de177de8ba0d157a08ef356
fool1280 <anhnguyen@Anhs-MBP.attlocal.net>
2020-05-25 03:18:43 -0500

    update using class component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+/*
 import React from 'react';
 import './App.css';
 import ChoiceCard from './components/ChoiceCard';
@@ -18,4 +20,27 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+import React, { Component } from 'react'
+import ChoiceCard from './components/ChoiceCard'
+import './App.css'
+
+const choices = {
+  rock:
+    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
+  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
+  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+};
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ChoiceCard name="You" winner={false} imgURL={choices.rock}/>
+        <ChoiceCard name="Computer" winner={true} imgURL={choices.paper}/>
+      </div>
+    )
+  }
+}
+
--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -1,4 +1,5 @@
-import React from 'react'
+/*import React from 'react'
+
 
 export default function ChoiceCard(props) {
     console.log("Props:", props);
@@ -9,4 +10,19 @@ export default function ChoiceCard(props) {
             <h3>{props.winner ? "Won" : "Loss"}</h3>
         </div>
     )
+}*/
+
+import React, { Component } from 'react'
+
+export default class ChoiceCard extends Component {
+    render() {
+        return (
+            <div className={`choice-card ${this.props.winner ? "winner" : "loser"}`}>
+                <h1>{this.props.name}</h1>
+                <img src={this.props.imgURL}/>
+                <h3>{this.props.winner ? "Won" : "Loss"}</h3>
+            </div>
+        )
+    }
 }
+
